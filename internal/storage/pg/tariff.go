bc9867c5f61b44c8951b4f8e4f83521e9de9eac4
fenek-dev <makstreonin@gmail.com>
2024-08-04 14:40:56 +0300

    Fix Tariff markup

---
+++ internal/storage/pg/tariff.go
@@ -0,0 +1,33 @@
+package pg
+
+import (
+	"context"
+	"github.com/fenek-dev/go-outline-bot/internal/models"
+	"github.com/georgysavva/scany/v2/pgxscan"
+)
+
+func (p *Postgres) GetTariff(ctx context.Context, tariffID uint64) (*models.Tariff, error) {
+
+	tariff := &models.Tariff{}
+	err := pgxscan.Get(ctx, p.conn, tariff, "SELECT * FROM tariffs WHERE id = $1", tariffID)
+	if err != nil {
+		return nil, err
+	}
+
+	return tariff, err
+}
+
+func (p *Postgres) GetTariffsByServer(ctx context.Context, serverId uint64) (tariffs []*models.Tariff, err error) {
+
+	rows, err := p.conn.Query(ctx, "SELECT * FROM tariffs WHERE server_id = $1", serverId)
+	if err != nil {
+		return nil, err
+	}
+
+	err = pgxscan.ScanAll(&tariffs, rows)
+	if err != nil {
+		return nil, err
+	}
+
+	return tariffs, err
+}
---
+++ internal/telegram/handlers/tariff.go
@@ -0,0 +1,78 @@
+package handlers
+
+import (
+	"context"
+	"fmt"
+	"github.com/fenek-dev/go-outline-bot/internal/telegram/markup"
+	"gopkg.in/telebot.v3"
+	"strconv"
+	"time"
+)
+
+func (h *Handlers) OpenTariffsMenu(c telebot.Context) error {
+	ctx, cancel := context.WithTimeout(context.Background(), h.timeout)
+	defer cancel()
+
+	serverID := c.Data()
+
+	id, err := strconv.ParseUint(serverID, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	tariffs, err := h.service.GetTariffsByServer(ctx, id)
+
+	rows := make([]telebot.Row, 0, len(tariffs))
+
+	for _, tariff := range tariffs {
+		if !tariff.Active {
+			continue
+		}
+		btn := telebot.Btn{
+			Text:   fmt.Sprintf("%s, %vGB, %v₽/%vд", tariff.Name, tariff.Bandwidth, tariff.Price, tariff.Duration),
+			Data:   strconv.FormatUint(tariff.ID, 10),
+			Unique: markup.TariffItem.Unique,
+		}
+
+		rows = append(rows, markup.TariffsMenu.Row(btn))
+	}
+
+	rows = append(rows, markup.TariffsMenu.Row(markup.TariffsBackBtn))
+
+	markup.TariffsMenu.Inline(
+		rows...,
+	)
+
+	return c.Edit("Тарифы:", markup.TariffsMenu)
+}
+
+func (h *Handlers) BackTariffsMenu(c telebot.Context) error {
+	return c.Edit("Сервера:", markup.ServersMenu)
+}
+
+func (h *Handlers) OpenTariff(c telebot.Context) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Hour)
+	defer cancel()
+
+	tariffID := c.Data()
+
+	id, err := strconv.ParseUint(tariffID, 10, 64)
+	if err != nil {
+		return err
+	}
+
+	tariff, err := h.service.GetTariff(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	err = c.Delete()
+	if err != nil {
+		return err
+	}
+	return c.Send(fmt.Sprintf("Тариф %s, %vGB, %v₽/%vд", tariff.Name, tariff.Bandwidth, tariff.Price, tariff.Duration), markup.TariffInfo)
+}
+
+func (h *Handlers) BackTariff(c telebot.Context) error {
+	return c.Edit("Тарифы:", markup.TariffsMenu)
+}
